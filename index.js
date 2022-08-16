664bb1a342b9ee1fbde0c58f644237c89f1086bc
Alena Okolike <95056164+summyalena@users.noreply.github.com>
2022-08-16 17:20:56 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let count = 0;
+let total = 0;
+function stringLength(string){
+    for( i = 0; i < string.length; i ++) {
+      count = count + string[i];
+    } return;
+}
+
+test('string character count'), () => {
+expect(stringLength).toBe(string.length)
+}
